a1ab373f740053bc712341d97e8b7b74154fb0ba
Paul Pechin <ppechin@usnews.com>
2018-04-25 21:30:09 -0500

    Test.

--- src/test.js
+++ src/test.js
@@ -1,9 +1,11 @@
 const moment = require('moment');
 
 exports.handler = async (event, context, callback) => {
-    const data = JSON.parse(event.body);
+    // const data = JSON.parse(event.body);
+
     const returnTime = moment().format('MM/DD/YYYY HH:mm:ss');
-    console.info('calling lambda function with', data);
+    console.info('calling lambda function with', event);
+    console.info('and context', context);
 
     callback(null, {
         statusCode: 200,
