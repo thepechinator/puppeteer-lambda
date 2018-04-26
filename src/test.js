2ff0fa640da08d0497e4f6e260bcbff536c1256c
Paul Pechin <ppechin@usnews.com>
2018-04-25 20:00:02 -0500

    Forgot to invoke callback in lambda function.

--- src/test.js
+++ src/test.js
@@ -4,5 +4,9 @@ exports.handler = async (event, context, callback) => {
     const data = JSON.parse(event.body);
     const returnTime = moment().format('MM/DD/YYYY HH:mm:ss');
     console.info('calling lambda function with', data);
-    return { status: 200, data: { returnTime } };
+
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ status: 200, data: { returnTime } }),
+    });
 };
