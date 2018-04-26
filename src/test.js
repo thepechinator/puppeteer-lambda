2347af9fe395afa19860a69cf2780579fc8533c8
Paul Pechin <ppechin@usnews.com>
2018-04-25 21:58:08 -0500

    More testing.

--- src/test.js
+++ src/test.js
@@ -2,13 +2,17 @@ const moment = require('moment');
 
 exports.handler = async (event, context, callback) => {
     // const data = JSON.parse(event.body);
+    const { debugId, delay } = event.data;
 
-    const returnTime = moment().format('MM/DD/YYYY HH:mm:ss');
-    console.info('calling lambda function with', event);
-    console.info('and context', context);
+    console.info(debugId, 'calling lambda function with', event);
+    console.info(debugId, 'and context', context);
 
-    callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ status: 200, data: { returnTime } }),
-    });
+    setTimeout(() => {
+        console.info(debugId, 'delay ended. returning');
+        const returnTime = moment().format('MM/DD/YYYY HH:mm:ss');
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ status: 200, data: { returnTime } }),
+        });
+    }, delay);
 };
