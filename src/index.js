047402147be5a549f8d5b7977c2ff07cb1714bf8
Paul Pechin <ppechin@usnews.com>
2018-05-02 12:11:12 -0500

    Add hide and remove selector functionality.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const setup = require('./starter-kit/setup');
 const {uploadToS3} = require('./starter-kit/uploader');
 const imageDiff = require('./starter-kit/image-diff');
 
+
 // exports.handler = async (event, context, callback) => {
 //   // for calls hitting the api gateway
 //   const { id, clientInvokeTime } = JSON.parse(event.body);
@@ -25,7 +26,8 @@ const imageDiff = require('./starter-kit/image-diff');
 // };
 
 exports.handler = async (event, context, callback) => {
-  const { url, snapshotIdentifier, debugId, baselineBase64String, viewport, config } = JSON.parse(event.body);
+  const { url, snapshotIdentifier, debugId, baselineBase64String, viewport, config } 
+    = JSON.parse(event.body);
   const startTime = Date.now();
   console.info(debugId, 'received url', url, 'and snapshotIdentifier', snapshotIdentifier);
   console.info(debugId, 'with config', config);
@@ -40,8 +42,11 @@ exports.handler = async (event, context, callback) => {
       baselineBase64String,
       viewport,
       startTime,
+      // Other config options we may need
       config,
     }).then(
+    // On a successful run, we invoke the callback with what data we want to
+    // send back
     (result) => callback(null, {
       statusCode: 200,
       body: JSON.stringify({status: 200, result})})
@@ -49,6 +54,8 @@ exports.handler = async (event, context, callback) => {
     (err) => {
       console.info(debugId, 'ran into error');
       console.info(err);
+      // Any runtime errors we log as a 500. Other error codes will be handled
+      // by the API gateway, outside of this lambda call.
       callback(null, 
         { 
           statusCode: 500, 
@@ -63,41 +70,92 @@ exports.run = async (browser,
   {
     url,
     snapshotIdentifier,
+    // A unique id we pass up from the client 
     debugId,
+    // Existing baseline image from client (if exists)
     baselineBase64String,
     viewport = { width: 1024, height: 768 },
+    // startTime comes from the client for debugging purposes. May not be needed eventually.
     startTime,
     // will contain some stuff we don't need for now,
     // but has some stuff we want
+    // This should be a merged config object of the global and a test's settings
     config,
   } = {}) => {
-  // implement here
-  // this is sample
+  const { hideSelectors, removeSelectors } = config;
+
   console.info(debugId, 'opening new page..');
   console.info(debugId, 'browser', browser);
   const page = await browser.newPage();
 
+  // debugging purposes
   if (config.intentionallyCauseError) {
     page.setViewPort1221();
   }
   // change to the right resolution
   await page.setViewport({width: viewport.width, height: viewport.height});
-  console.info(debugId, 'trying to go to page...');
   // wait for the right event
   // waitUntil: 'networkidle0', 
   // waitUntil: 'networkidle2',
   // https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegotourl-options
+  // waitUntil defaults to 'load'
   await page.goto(url, {timeout: config.timeout});
   // console.log((await page.content()).slice(0, 500));
-  console.info(debugId, 'past page load..');
-  // await page.type('#lst-ib', 'aaaaa');
-  // // avoid to timeout waitForNavigation() after click()
-  // await Promise.all([
-  //   // avoid to
-  //   // 'Cannot find context with specified id undefined' for localStorage
-  //   page.waitForNavigation(),
-  //   page.click('[name=btnK]'),
-  // ]);
+  console.info(debugId, 'Loaded page');
+
+  // Now we need to wait for a certain element so we may hide or remove it.
+
+  // hide or display none element on page
+  // for hiding
+  (hideSelectors.length || removeSelectors.length) && await page.evaluate((hideSelectors, removeSelectors) => {
+    for (const selector of hideSelectors) {
+      // eslint-disable-next-line
+      const el = document.querySelector(selector);
+      if (el) {
+        el.style.visibility = 'hidden';
+      }
+    }
+
+    for (const selector of removeSelectors) {
+      // eslint-disable-next-line
+      const el = document.querySelector(selector);
+
+      if (el) {
+        el.style.display = 'none';
+      }
+    }
+  }, hideSelectors, removeSelectors);
+  console.info(debugId, 'Processed hide and remove selectors');
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
+  // await page.goto('https://s3.amazonaws.com/usn-static-atlas-mocks-test-bucket/sand32/1525106539885/mocks/pages/education/higher-education/grad/landing.html');
+  // const el = await page.$('.widget-bleed');
+  // hide element
+  // await page.evaluate(el => el.style.visibility = 'hidden', el);
+  // remove element
+  // await page.evaluate(el => el.style.display = 'none', el);
+  // await page.screenshot({ path: 'example.png', fullPage: true })
+  // await browser.close();
+
+  // Long-term goal: Also need a way to target certain selectors (unfortunately, targeting a selector means
+  // running 1 x viewport count per selector... so 3 lambda invocations).
+
+  // Take screenshot of specific element.
+  //
+  // const browser = await puppeteer.launch();
+  // const page = await browser.newPage();
+  // await page.goto('https://s3.amazonaws.com/usn-static-atlas-mocks-test-bucket/sand32/1525106539885/mocks/pages/education/higher-education/grad/landing.html');
+  // if outside viewport, element will generate blank screenshot... so we need
+  // to increase the viewport height so it is in view:
+  // tracked in https://github.com/GoogleChrome/puppeteer/issues/2423
+  //
+  // await page.setViewport({ width: 800, height: 10000 })
+  // const el = await page.$('.widget-bleed');
+  // await el.screenshot({ path: 'example.png'  });
+  // await browser.close();
+
+  // Long-term goal: if a lambda times out, it 502s... and we don't have a good way to handle that... yet.
+
   console.info(debugId, 'trying to take a screenshot');
   await page.waitFor(config.screenshotDelay);
   await page.screenshot({
@@ -118,7 +176,6 @@ exports.run = async (browser,
 
   // const aws = require('aws-sdk');
   // const s3 = new aws.S3({apiVersion: '2006-03-01'});
-  console.info(debugId, 'trying to read from the screenshot file');
   // const fs = require('fs');
 
   let screenshot = null;
@@ -158,7 +215,7 @@ exports.run = async (browser,
 
   let resultObject = {
     id: snapshotIdentifier,
-    // the same as the identifier, for now
+    // the same as the identifier
     // actual path resolution will be done by the reporter and importer
     baselineFileName: `${snapshotIdentifier}.${screenshotContentType}`,
     testScreenshotPath: screenshotPath,
